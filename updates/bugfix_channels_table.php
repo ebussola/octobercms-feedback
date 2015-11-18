9bb9b06a6be4b2696a1cd05518ff664cff81a254
Leonardo Shinagawa <leonardo@ebussola.com>
2015-11-18 16:41:31 -0200

    Bugfix of column method_data. varchar(255) was not enough changed to longtext

---
+++ updates/bugfix_channels_table.php
@@ -0,0 +1,26 @@
+<?php namespace eBussola\Feedback\Updates;
+
+use Illuminate\Database\Schema\Blueprint;
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class CreateChannelsTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('ebussola_feedback_channels', function(Blueprint $table)
+        {
+            $table->longText('method_data')->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('ebussola_feedback_channels', function(Blueprint $table)
+        {
+            $table->string('method_data')->change();
+        });
+    }
+
+}
